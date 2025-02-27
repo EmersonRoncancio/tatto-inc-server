ef3f248ac457b93793f4f31a9226b296654ac652
Emerson Roncancio <roncancioavila@gmail.com>
2025-02-27 14:36:59 -0500

    Schemas de usuarios y tatuadores

--- src/auth/entities/tattoo-artist.entity.ts
+++ src/auth/entities/tattoo-artist.entity.ts
@@ -1,3 +1,66 @@
+import { Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export class TattooArtist extends Document {}
+export class TattooArtist extends Document {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    index: true,
+  })
+  specialty: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  expereince: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+  })
+  instagram: string;
+
+  @Prop({
+    type: String,
+  })
+  facebook: string;
+
+  @Prop({
+    type: String,
+  })
+  twitter: string;
+
+  @Prop({
+    type: String,
+  })
+  tiktok: string;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: String,
+  })
+  description: string;
+}
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,3 +1,30 @@
+import { Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export class User extends Document {}
+export class User extends Document {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: Date,
+    default: Date.now,
+  })
+  registrationDate: Date;
+}
